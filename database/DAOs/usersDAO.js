1e37258e1637985b046b58d003064adb00eb67df
youms <yussef.msabeh@gmail.com>
2023-01-24 21:29:16 +0200

    feat: handle update user information (firstName, lastName ,userName ,password)

--- database/DAOs/usersDAO.js
+++ database/DAOs/usersDAO.js
@@ -13,12 +13,59 @@ export async function signUp(user) {
     );
   } catch (e) {
     console.error(e);
+    throw e;
   }
 }
 
 export async function checkUserName(userName) {
-  return await db.get(
-    "SELECT id,firstName,lastName,userName,password FROM users WHERE userName=?",
-    userName
-  );
+  try {
+    return await db.get(
+      "SELECT id,firstName,lastName,userName,password FROM users WHERE userName=?",
+      userName
+    );
+  } catch (e) {
+    throw e;
+  }
+}
+
+export async function getUserById(id) {
+  try {
+    return await db.get("SELECT password FROM users WHERE id=? ", id);
+  } catch (e) {
+    throw e;
+  }
+}
+
+export async function updateUser(user) {
+  try {
+    await db.run(
+      "UPDATE users SET firstName=?,lastName=? WHERE id=?",
+      user.firstName,
+      user.lastName,
+      user.userId
+    );
+  } catch (e) {
+    throw e;
+  }
+}
+
+export async function updateUserName(user) {
+  try {
+    await db.run(
+      "UPDATE users SET userName=? WHERE id=?",
+      user.userName,
+      user.userId
+    );
+  } catch (e) {
+    throw e;
+  }
+}
+
+export async function updatePassword(id, password) {
+  try {
+    console.log(id, password);
+    await db.run("UPDATE users SET password=? WHERE id=?", password, id);
+  } catch (e) {
+    throw e;
+  }
 }
--- endPointHandlers/users.js
+++ endPointHandlers/users.js
@@ -2,6 +2,7 @@ import * as usersDAO from "../database/DAOs/usersDAO.js";
 import * as crypto from "node:crypto";
 import * as dotenv from "dotenv";
 import jwt from "jsonwebtoken";
+import { db } from "../database/index.js";
 
 dotenv.config();
 export async function signUp(request, response) {
@@ -62,6 +63,78 @@ export async function login(request, response) {
       userName: user.userName,
       userToken: token,
     });
+  } catch (e) {
+    console.error(e);
+    response.sendStatus(500);
+  }
+}
+
+export async function updateUser(request, response) {
+  try {
+    let user = request.body;
+    console.log(user);
+    if (!user.firstName || !user.lastName) {
+      response.sendStatus(400);
+      return;
+    }
+    if (await usersDAO.checkUserName(user.userName)) {
+      response.status(400).send("userName already exist");
+      return;
+    }
+    await usersDAO.updateUser(user);
+    response.status(200).send("user updated successfully");
+  } catch (e) {
+    console.error(e);
+    response.sendStatus(500);
+  }
+}
+
+export async function updateUserName(request, response) {
+  try {
+    let user = request.body;
+    if (!user.userName) {
+      request.status(401).send("userName required");
+    }
+    let userFromDatabase = await usersDAO.checkUserName(user.userName);
+    if (!userFromDatabase) {
+      await usersDAO.updateUserName(user);
+      response.status(200).send("userName updated successfully");
+      return;
+    }
+    if (userFromDatabase.id == user.userId) {
+      response.status(200).send("userName updated successfully");
+      return;
+    }
+    if (userFromDatabase) {
+      response.status(400).send("userName already exist");
+      return;
+    }
+  } catch (e) {
+    console.error(e);
+    response.sendStatus(500);
+  }
+}
+
+export async function updatePassword(request, response) {
+  try {
+    let body = request.body;
+    console.log(body);
+
+    if (!body.newPassword || !body.confirmPassword || !body.oldPassword) {
+      response.sendStatus(400);
+      return;
+    }
+    if (body.newPassword != body.confirmPassword) {
+      response.status(400).send("Password does not match");
+      return;
+    }
+    let { password } = await usersDAO.getUserById(body.userId);
+    if (password != body.oldPassword) {
+      response.status(401).send("Incorrect password");
+      return;
+    }
+    await usersDAO.updatePassword(body.userId, body.newPassword);
+    response.sendStatus(200);
   } catch (e) {
     console.error(e);
     response.sendStatus(500);
