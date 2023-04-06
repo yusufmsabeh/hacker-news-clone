5d9fd19e1a9bbc4a603447b46c26651fe73827be
Yusuf Msabeh <yussef.msabeh@gmail.com>
2023-04-06 18:48:14 +0300

    feat:read data from form-data

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import express from "express";
+import bodyParser from "body-parser";
+import multer from "multer";
 import { openDatabase } from "./database/index.js";
 import { router as userRouter } from "./routes/user.js";
 import { router as postRouter } from "./routes/post.js";
 const app = express();
+const upload = multer();
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(upload.array());
 app.listen(process.env.PORT);
 
 openDatabase();
